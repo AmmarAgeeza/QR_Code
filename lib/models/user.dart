4f13b52bad5994ac927f41ffb8989166ee935f42
AYaqw <ayhb756@gmail.com>
2022-08-07 10:10:51 +0200

    3- add configure + init

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class UserFields {
+  static const String id = '_ID';
+  static const String name = '_Name';
+  static const String email = '_Email';
+  static const String day1 = '_Day1';
+  static const String day2 = '_Day2';
+  static const String day3 = '_Day3';
+  static const String day4 = '_Day4';
+  static const String day5 = '_Day5';
+
+  static List<String> getFields() =>
+      [id, name, email, day1, day2, day3, day4, day5];
+}
